7ccecb5f6c878954cdd3d39068ca87c08464cb28
Sweeper777 <sumulang@gmail.com>
2016-04-18 12:48:40 +0800

    fixed data migration problem

--- ColorBible/ColorNamePair.swift
+++ ColorBible/ColorNamePair.swift
@@ -3,15 +3,13 @@ import CoreData
 
 
 class ColorNamePair: NSManagedObject {
-
     override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
         super.init(entity: entity, insertIntoManagedObjectContext: context)
     }
     
-    convenience init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext, colorValue: Int32, colorName: String) {
+    convenience init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext, color: Int32, colorName: String) {
         self.init(entity: entity, insertIntoManagedObjectContext: context)
-        self.colorValue = NSNumber(int: colorValue)
+        self.colorValue = NSNumber(int: color)
         self.colorName = colorName
     }
-
 }
